668d17cbdd0c0acc807f1db68ab2fe193a62c6ae
BLacey99 <81555350+BLacey99@users.noreply.github.com>
2022-01-20 15:49:44 -0600

    Add files via upload
    Added functionality to rock, paper, and scissors input buttons. Plays in console but without prompt.
    
    Need to fix: scripts.js:68 Uncaught RangeError: Maximum call stack size exceeded -- Occurs on first click when page first loads but does not occur again until page is refreshed.
    
    Start button no longer works, removed function temporarily while reworking JS code.
    
    TODO:
    1. Add limiter to number of rounds allowed to be played. Limiting to 5.
    2. Add incremental counter which awards points to computer or player based on answers.
    3. Declare winner with whoever has the most points.
    4. Possible implementation to award points to both the player and computer on draw, or award no points in the event of a draw and decrement the incrementation by 1 in order to allow for another round to be played until one of the two have the majority of points.

--- scripts.js
+++ scripts.js
@@ -1,141 +1,118 @@
+let i = 0;
+let choice;
 
-
-
-/*
-function rockValue() {
-    console.log("rock");
-    choice = "rock";
-    return "rock";
-    counter();
-    i++;
-    console.log(i);
-    playRound(playerSelection, computerSelection);
+if(i>5){
+    console.log("Finished");
 }
-
-function paperValue() {
-    console.log("paper");
-    choice === "paper";
-    return "paper";
-    i++;
-    console.log(i);
-    playRound(playerSelection, computerSelection);
-}
-
-function scissorsValue() {
-    console.log("scissors");
-    choice === "scissors";
-    return "scissors"
-    i++;
-    console.log(i);
-    playRound(playerSelection, computerSelection);
-}
-*/
-
-function game() {
-    console.clear();
-
-    for (i = 0; i < 5; i++) {
-
-        {
-
-
-            function computerPlay() {
-                const playMaker = Math.floor(Math.random() * 3)
-                if (playMaker === 0) {
-                    console.log("The computer chose rock")
-                    return "rock";
-                } else if (playMaker === 1) {
-                    console.log("The computer chose paper")
-                    return "paper";
-                } else if (playMaker === 2) {
-                    console.log("The computer chose scissors")
-                    return "scissors";
-                }
-            }
-
-            function humanPlay() {
-                let choice = prompt("Please type either rock, paper, or scissors.");
-                choice.toLowerCase();
-
-                console.log(`You have chosen:${choice}.`);
-                if (choice === "rock") {
-                    return "rock";
-                } else if (choice === "paper") {
-                    return "paper";
-                } else if (choice === "scissors") {
-                    return "scissors";
-                } else {
-                    alert("Error, please enter an answer.");
-                }
-            }
-
-
-
-
-
-            let playerSelection = humanPlay();
-            let computerSelection = computerPlay();
-
-
-
-            function playRound(playerSelection, computerSelection) {
-
-                switch (playerSelection) {
-                    case "rock":
-                        if (computerSelection === "rock") {
-                            console.log("Draw.");
-                            return "Draw.";
-                        } else if (computerSelection === "paper") {
-                            console.log("Computer victory.");
-                            return "Computer victory.";
-                        } else if (computerSelection === "scissors") {
-                            console.log("Human victory.");
-                            return "Human victory.";
-                        }
-
-                        break;
-                    case "paper":
-                        if (computerSelection === "rock") {
-                            console.log("Human victory.");
-                            return "Human victory.";
-                        } else if (computerSelection === "paper") {
-                            console.log("Draw.");
-                            return "Draw.";
-                        } else if (computerSelection === "scissors") {
-                            console.log("Computer victory.");
-                            return "Computer victory.";
-                        }
-
-                        break;
-                    case "scissors":
-                        if (computerSelection === "rock") {
-                            console.log("Computer victory.");
-                            return "Computer victory.";
-                        } else if (computerSelection === "paper") {
-                            console.log("Human victory.");
-                            return "Human victory.";
-                        } else if (computerSelection === "scissors") {
-                            console.log("Draw");
-                            return "Draw.";
-                        }
-
-                        break;
-                }
-
-            }
-            playRound(playerSelection, computerSelection);
-        }
-
-    }
-}
-
 
 function reset() {
     i = 0;
     console.clear();
 }
+function rock() {
+    let playerSelection = humanPlay(choice);
+    let computerSelection = computerPlay();
+    choice = "rock";
+    i++;
+    console.log(i);
+    playRound(playerSelection, computerSelection);
+    return "rock";
+}
+function paper() {
+    let playerSelection = humanPlay(choice);
+    let computerSelection = computerPlay();
+    choice = "paper";
+    i++;
+    console.log(i);
+    playRound(playerSelection, computerSelection);
+    return "paper";
+}
+
+function scissors() {
+    choice = "scissors";
+    i++;
+    console.log(i);
+    let playerSelection = humanPlay(choice);
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    return "scissors";
+}
+
+function computerPlay() {
+    const playMaker = Math.floor(Math.random() * 3)
+    if (playMaker === 0) {
+        console.log("The computer chose rock")
+        return "rock";
+    } else if (playMaker === 1) {
+        console.log("The computer chose paper")
+        return "paper";
+    } else if (playMaker === 2) {
+        console.log("The computer chose scissors")
+        return "scissors";
+    }
+}
+
+function humanPlay(choice) {
+    console.log(`You have chosen:${choice}.`);
+    if (choice === "rock") {
+        return "rock";
+    } else if (choice === "paper") {
+        return "paper";
+    } else if (choice === "scissors") {
+        return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "rock") {
+                console.log("Draw.");
+                return "Draw.";
+            } else if (computerSelection === "paper") {
+                console.log("Computer victory.");
+                return "Computer victory.";
+            } else if (computerSelection === "scissors") {
+                console.log("Human victory.");
+                return "Human victory.";
+            }
+
+            break;
+        case "paper":
+            if (computerSelection === "rock") {
+                console.log("Human victory.");
+                return "Human victory.";
+            } else if (computerSelection === "paper") {
+                console.log("Draw.");
+                return "Draw.";
+            } else if (computerSelection === "scissors") {
+                console.log("Computer victory.");
+                return "Computer victory.";
+            }
+
+            break;
+        case "scissors":
+            if (computerSelection === "rock") {
+                console.log("Computer victory.");
+                return "Computer victory.";
+            } else if (computerSelection === "paper") {
+                console.log("Human victory.");
+                return "Human victory.";
+            } else if (computerSelection === "scissors") {
+                console.log("Draw");
+                return "Draw.";
+            }
+
+            break;
+    }
 
 
+    playRound(playerSelection, computerSelection);
 
+   // function game() {
+       // console.clear();
+
+    }
 
 
